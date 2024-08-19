d7455058bd532b76405c1e01a1cff0765028efed
James <james@jameshost.net>
2024-08-19 10:21:24 -0700

    update

--- src/NumberLooperAddRander.java
+++ src/NumberLooperAddRander.java
@@ -1,11 +1,15 @@
+import java.lang.Math;
 
 public class NumberLooperAddRander {
 
 	public static void main(String[] args) {
-		int CurrentNumber = 1;
-		for (int i = 0; i < 100; i++) {
+		double CurrentNumber = 0;
+		for (int i = 0; i < 1000; i++) {
 			System.out.println(CurrentNumber);
-			CurrentNumber = CurrentNumber+1;
+			Double IncrementAmount = Math.random();
+			//var y = (" "+IncrementAmount.toString()+" ");
+			//System.out.print(y);
+			CurrentNumber = CurrentNumber+IncrementAmount;
 		}
 	}
 
