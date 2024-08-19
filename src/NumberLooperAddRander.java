fa995073ccdaeda114325b3ab35924349fc2716b
James <james@jameshost.net>
2024-08-18 17:46:49 -0700

    incremental work no description

---
+++ src/NumberLooperAddRander.java
@@ -0,0 +1,12 @@
+
+public class NumberLooperAddRander {
+
+	public static void main(String[] args) {
+		int CurrentNumber = 1;
+		for (int i = 0; i < 100; i++) {
+			System.out.println(CurrentNumber);
+			CurrentNumber = CurrentNumber+1;
+		}
+	}
+
+}
